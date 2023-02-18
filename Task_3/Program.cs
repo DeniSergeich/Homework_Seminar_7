9cf0aa761594e1fe3318dc4d2504fc4d6cfd3109
Denis <damned_wolf@mail.ru>
2023-02-18 14:30:27 +0300

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+int[,] array = FillArray(rows, columns);
+PrintArray(array);
+WriteLine();
+Write($"Среднее арифмитическое столбцов массива равно {String.Join("; ", GetResult(array))}");
+
+
+int[,] FillArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+double[] GetResult(int[,] array)
+{
+
+    double[] resultArray = new double[array.GetLength(1)];
+
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum += array[j, i];
+        }
+        resultArray[i] = Math.Round((sum / array.GetLength(0)), 1);
+    }
+    return resultArray;
+}
